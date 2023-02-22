df4cc4bde7eb3671dd7c7ee8c56386caa44446d1
Raquell Sanntos <santos.soares0424@gmail.com>
2023-02-22 17:01:43 -0400

    Adicionando  refatoração de código para facilitar manutenção e teste

--- app/src/main/java/com/softwaresolutions/sorteadoads/SecondSemesterActivity.kt
+++ app/src/main/java/com/softwaresolutions/sorteadoads/SecondSemesterActivity.kt
@@ -2,10 +2,16 @@ package com.softwaresolutions.sorteadoads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first_semester.*
+import kotlinx.android.synthetic.main.activity_second_semester.*
 
 class SecondSemesterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_semester)
+
+        bt_secondary.setOnClickListener {
+            tx_sorteado2.setText("A lista de alunos está vazia!")
+        }
     }
 }
